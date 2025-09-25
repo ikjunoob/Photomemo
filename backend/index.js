2e13c5d4f3b95317951dfa80500000bff3f2c103
KIJ <dlrwn0730@naver.com>
2025-09-25 19:21:09 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,16 +16,16 @@ app.use(express.json());
 
 // MongoDB에 연결합니다.
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB 연결 성공"))
-  .catch((err) => console.error("MongoDB 연결 실패:", err.message));
+    .then(() => console.log("MongoDB 연결 성공"))
+    .catch((err) => console.error("MongoDB 연결 실패:", err.message));
 
 // 기본 GET 라우트 설정
 // 서버의 루트 URL('/')로 GET 요청이 오면 "API is running..." 메시지를 응답합니다.
 app.get("/", (req, res) => {
-  res.send("API is running...");
+    res.send("Photomemo!");
 });
 
 // 지정된 PORT에서 서버를 실행합니다.
 app.listen(PORT, () => {
-  console.log(`${PORT}번 포트에서 서버가 실행되었습니다.`);
+    console.log(`${PORT}번 포트에서 서버가 실행되었습니다.`);
 });
