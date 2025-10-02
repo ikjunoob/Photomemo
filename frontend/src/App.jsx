7000a017ad47f639603f9a060edd2aecd3dbd054
KIJ <dlrwn0730@naver.com>
2025-10-02 18:27:25 +0900

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import './App.scss'
+
 import Landing from './pages/Landing'
+import AdminAuthPanel from './components/AuthPanel'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Landing />
-    </>
+    <div className='page'>
+      <Routes>
+        <Route path='/' element={<Lankding/>}/>
+        <Route path='/admin/login' element={<AuthPanel/>}/>
+      </Routes>
+      
+    </div>
   )
 }
 
